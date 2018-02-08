4810b33e0f86d1a7ccd1e958447852dbab271860
Yaokun Pan <myron.yk.pan@gmail.com>
2018-02-08 11:11:30 +1100

    Added back top

--- src/app/owl/owl.module.ts
+++ src/app/owl/owl.module.ts
@@ -9,19 +9,20 @@ import { OwlNGRoutingModule } from './owl.routing';
 import { OwlNGStartComponent } from './getting-start.component';
 import { OwlNGIntroductionComponent } from './owl-introduction.component';
 import { OwlIntroductionService } from './owl-introduction.service';
-
-import { OwlRippleModule } from '../../../npmdist/components/ripple';
-import { OwlTabsModule } from '../../../npmdist/components/tabs';
 import { CodeHighlightDirective } from './code-highlight.directive';
-
 import { OwlNGRippleComponent } from './ripple/ripple.component';
 import { OwlNGTabsComponent } from './tabs/tabs.component';
+
+import { OwlBackTopBtnModule } from '../../../npmdist/components/backTop';
+import { OwlRippleModule } from '../../../npmdist/components/ripple';
+import { OwlTabsModule } from '../../../npmdist/components/tabs';
 
 @NgModule({
     imports: [
         CommonModule,
         OwlNGRoutingModule,
 
+        OwlBackTopBtnModule,
         OwlRippleModule,
         OwlTabsModule,
     ],
