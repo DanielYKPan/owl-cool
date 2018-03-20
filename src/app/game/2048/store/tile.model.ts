1028f5e8337836659e8327f8e64012e230253388
Yaokun Pan <myron.yk.pan@gmail.com>
2018-03-20 17:15:07 +1100

    Used share uuid

--- src/app/game/2048/store/tile.model.ts
+++ src/app/game/2048/store/tile.model.ts
@@ -1,6 +1,7 @@
 /**
  * tile.model
  */
+import { uuid } from '../../share/utils';
 
 export class Tile {
     public id: string;
@@ -11,25 +12,10 @@ export class Tile {
 
     constructor( position: number = null, value: number = 2 ) {
 
-        this.id = this.uuid();
+        this.id = uuid();
         this.value = value;
         this.position = position;
         this.merged = false;
         this.shouldDump = false;
-    }
-
-    private uuid(): string {
-        let result = '';
-
-        for (let i = 0; i < 32; i++) {
-            const random = Math.random() * 16 || 0;
-            if (i === 8 || i === 12 || i === 16 || i === 20) {
-                result += '-';
-            }
-            result += (i === 12 ? 4 : (i === 16 ? (random && 3 || 8) : random))
-                .toString(16);
-        }
-
-        return result;
     }
 }
